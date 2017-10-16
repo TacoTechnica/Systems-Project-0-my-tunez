6bebda2471f2720ae07532caf52aa628b5f93950
adrisj7 <adrisj7@gmail.com>
2017-10-16 15:31:40 -0400

    Add extra header functions for linked list

--- linked_list.h
+++ linked_list.h
@@ -1,9 +1,11 @@
-struct node {int i; struct node * next;};
+struct song_node {char name[256]; char artist[256]; struct node * next;};
 
 
-void print_list(struct node *);
+void print_list(struct song_node *);
 
-struct node * insert_front(struct node *, int);
+// Pointers because we don't need our strings to be mutable
+struct song_node * insert_front(struct song_node *, char *name, char *artist);
 
-struct node * free_list(struct node *);
-	  
+struct song_node *insert_ordered(struct song_node *, char *name, char *artist);
+
+struct song_node * free_list(struct song_node *);
