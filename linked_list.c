2d45c7489577a5a501b053c154081f2321ca0dca
gnewman00 <gnewman@stuy.edu>
2017-10-16 15:23:03 -0400

    add comments to implementation

--- linked_list.c
+++ linked_list.c
@@ -3,30 +3,26 @@
 #include "linked_list.h"
 
 void print_list(struct node * head){
-  struct node * temp = head;
 
-  while (head){
-    printf("%d --> ", head->i);
-
-      head = head->next;
+  while (head){ 
+    printf("%d --> ", head->i); 
+    head = head->next; // move to the next node
     }
-  printf("NULL\n");
+  printf("NULL\n"); // print out the null at the end
 }
 
 struct node * insert_front(struct node * head, int cargo){
   struct node * new_head = malloc(sizeof(struct node)); 
-  new_head->i = cargo;
-  new_head->next = head;
-  return new_head;
+  new_head->i = cargo; /new head of list
 
 }
 
 struct node * free_list(struct node * head){
-  while (head){
-    struct node * temp = head;
-    head = head->next;
-    free(temp);
+  while (head){ 
+    struct node * temp = head; 
+    head = head->next; // move from current node to next node
+    free(temp); // free current node using temp pointer
   }
-  return head;
+  return head; // return null
 }
 	  
