cf12af1e6ab58989409bf6967b11d9403810c211
adrisj7 <adrisj7@gmail.com>
2017-10-17 15:11:01 -0400

    Clarify name for insert_ordered header

--- linked_list.h
+++ linked_list.h
@@ -6,6 +6,7 @@ void print_list(struct song_node *);
 // Pointers because we don't need our strings to be mutable
 struct song_node * insert_front(struct song_node *, char *name, char *artist);
 
-struct song_node *insert_ordered(struct song_node *, char *name, char *artist);
+struct song_node *insert_ordered(struct song_node * start, char *name, char *artist);
 
 struct song_node * free_list(struct song_node *);
+
